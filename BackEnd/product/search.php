492c1e70b9090743bdca3c005f893cc79d4afa36
Ciddarth Raaj <ciddarth@apples-MacBook-Air.local>
2020-01-14 03:18:58 +0530

    Completed 80%

---
+++ BackEnd/product/search.php
@@ -0,0 +1,15 @@
+<?php
+include '../login.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$query = 'SELECT * FROM product WHERE name LIKE "%'.$_GET['str'].'%"';
+$results = mysqli_query($db, $query);
+
+$response = [];
+
+while ($row = $results->fetch_assoc()) {
+    $response[] = $row;
+}
+
+echo json_encode($response);
