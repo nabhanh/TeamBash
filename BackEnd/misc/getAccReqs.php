39237c6c43042bbf4ebdd572c4697bef48140ab4
Nabhan Hanif <30296512+nabhanh@users.noreply.github.com>
2020-01-14 01:47:04 +0530

    Few Chnages

--- BackEnd/misc/getAccReqs.php
+++ BackEnd/misc/getAccReqs.php
@@ -3,7 +3,7 @@ include '../login.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$query = 'SELECT product.name, bidding.bid_price, bidding.bid_qty FROM product, bidding WHERE bidding.pid=product.id and bidding.status=1 and fid='.$_GET['fid'];
+$query = 'SELECT product.name,product.id, bidding.bid_price, bidding.bid_qty FROM product, bidding WHERE bidding.pid=product.id and bidding.status=1 and fid='.$_GET['fid'];
 $results = mysqli_query($db, $query);
 
 
