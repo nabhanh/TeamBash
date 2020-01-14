9ccf310a6eb403aa771edadc1434bbdf6a1d596f
Ciddarth Raaj <ciddarth@apples-MacBook-Air.local>
2020-01-14 07:21:36 +0530

    85% Done

--- BackEnd/misc/addSMSData.php
+++ BackEnd/misc/addSMSData.php
@@ -12,11 +12,11 @@ for($i = 0; $i < count($productArray); $i++){
     $productText =  explode(" ",$productArray[$i]);
 
     for($j = 0; $j < count($productText); $j+=3){
-        $str.="('".$productText[$j]."','".$productText[$j+1]."','".$productText[$j+2]."', 4),";
+        $str.="('".$productText[$j]."','".$productText[$j+1]."','".$productText[$j+2]."', 1, 1),";
     }
 }
 
-$query = "INSERT INTO product (name , quantity, price, fid) VALUES ";
+$query = "INSERT INTO product (name , quantity, price, fid, status) VALUES ";
 $query.=substr($str, 0, -1);
 error_log($query);
 mysqli_query($db, $query);
--- BackEnd/product/put_data.php
+++ BackEnd/product/put_data.php
@@ -3,7 +3,7 @@ include '../login.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$query = "INSERT INTO product ( name, quantity, price, fid) VALUES ( ?, ?, ?, ?)";
+$query = "INSERT INTO product ( name, quantity, price, fid, status) VALUES ( ?, ?, ?, ?, 1)";
 
 $bind = $db->prepare($query);
 $bind->bind_param('siii', $_GET['name'], $_GET['quantity'], $_GET['price'], $_GET['fid']);
