e3f227ca961690b1d839cd3ee04118e3cd8b4728
Ciddarth Raaj <ciddarth@apples-MacBook-Air.local>
2020-01-14 08:37:08 +0530

    Final Commit

--- BackEnd/bidding/getDataVendor.php
+++ BackEnd/bidding/getDataVendor.php
@@ -3,7 +3,7 @@ include '../login.php';
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$query = 'SELECT * FROM product WHERE status = 1 AND quantity != 0';
+$query = 'SELECT b.bid_price, b.bid_qty, f.name as fname, p.name as name FROM bidding as b, farmer as f, product as p WHERE b.status = 1 AND b.vid = '.$_GET['id'].' AND b.pid = p.id';
 $results = mysqli_query($db, $query);
 
 $response = [];
